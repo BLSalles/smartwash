a18fe06a90fd49a1447a398c844601f9afd26339
Bruno Salles <brx14.bruno@gmail.com>
2026-02-26 23:28:41 -0300

    Mensagem do commit2

--- agendamento/models.py
+++ agendamento/models.py
@@ -11,10 +11,10 @@ class Horario(models.Model):
 
 class Agendamento(models.Model):
 
-    nome = models.CharField(max_length=100)
-    email = models.EmailField()
-    whatsapp = models.CharField(max_length=20)
-    placa = models.CharField(max_length=10)
+    nome = models.CharField(max_length=100000)
+    email = models.EmailField(max_length=100000)
+    whatsapp = models.CharField(max_length=10000)
+    placa = models.CharField(max_length=100000)
 
     horario = models.OneToOneField(
         Horario,
